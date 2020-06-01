dc1cfd56c7ad21d6b36de637ac597d8b4d3e342f
Merculous <24739823+Merculous@users.noreply.github.com>
2020-06-01 15:05:49 -0700

    Add ldd-copy

--- README.md
+++ README.md
@@ -6,3 +6,7 @@ Uses xz, 7zip, and zpaq to achieve maximum compression
 
 Uses ffmpeg to convert any supported video to a h264, 1280x720, 60fps, 6Mbps, mp4 file. It uses motion interpolation (basically blends frames) to achieve
 60fps or as most call it, smooth motion.
+
+# ldd-copy
+
+Copies libraries used in a binary printed by ldd. Finds real path, which means it doesn't copy the symlink, and follows the symlink instead.
---
+++ ldd-copy.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+
+import argparse
+import os
+import shutil
+import subprocess
+import sys
+
+argv = sys.argv
+
+
+def getLibsFromFile(file, path):
+    if os.path.isfile(file):
+        # Get outputted libraries from ldd
+        cmd = subprocess.run((
+            'ldd',
+            file),
+            stdout=subprocess.PIPE,
+            universal_newlines=True)
+        files = cmd.stdout.splitlines()
+        for stuff in files:
+            orig = stuff.split('(')
+            new = orig[0].strip()
+            if '=>' in new:
+                # tmp is a list, 0 name of object/symlink, 1 path of used object
+                tmp = new.replace(' =>', '').split(' ')
+                # Fix path because most are symlinks, os.path.realpath will fix this
+                tmp[1] = os.path.realpath(tmp[1])
+                print('Copying object: {}'.format(tmp[1]))
+                shutil.copy(tmp[1], '{}/{}'.format(path, tmp[0]))
+    else:
+        raise IOError('{} is not a file!'.format(argv[2]))
+
+    if os.path.isdir(path):
+        print('Saving to directory: {}'.format(path))
+    else:
+        raise IOError('{} is not a directory!'.format(argv[3]))
+
+
+def main():
+
+    parser = argparse.ArgumentParser(usage='{} <file> <dir>'.format(argv[0]))
+
+    parser.add_argument('-i', nargs=2, metavar=('FILE', 'DIR'))
+
+    args = parser.parse_args()
+
+    if args.i:
+        getLibsFromFile(argv[2], argv[3])
+    else:
+        parser.print_help(sys.stderr)
+
+
+if __name__ == '__main__':
+    main()
